25ee728798ef6a8d19e72b53bdba9bdb13e59fd1
Jerome Kelleher <jk@well.ox.ac.uk>
2025-08-17 13:30:39 +0100

    Different approach to handling parallelism for rematching

--- arg_postprocessing/scripts/rematch_recombinants.py
+++ arg_postprocessing/scripts/rematch_recombinants.py
@@ -1,3 +1,5 @@
+import collections
+import random
 import concurrent.futures as cf
 import pathlib
 import tszip
@@ -37,8 +39,7 @@ def dump(json_data, path):
 @click.argument("ts")
 @click.argument("pattern")
 @click.argument("output")
-@click.option("--cores", type=int)
-def run(ts, pattern, output, cores):
+def run(ts, pattern, output):
     ts = tszip.load(ts)
 
     # First, make sure we've pushed up all unary recombinant nodes so we
@@ -48,26 +49,39 @@ def run(ts, pattern, output, cores):
     recomb_nodes = np.where(ts.nodes_flags & sc2ts.NODE_IS_RECOMBINANT > 0)[0]
     node_mutations = np.bincount(ts.mutations_node)
 
-    easy_work = []
-    hard_work = []
+    all_work = collections.defaultdict(list)
 
     for u in recomb_nodes:
         md = ts.node(u).metadata
         date = md["sc2ts"]["date_added"]
         muts = node_mutations[u]
-        if muts <= 2:
-            easy_work.append(Work(pattern, u, date))
-        else:
-            hard_work.append(Work(pattern, u, date))
+        all_work[muts].append(Work(pattern, u, date))
 
+    random.seed(42)
+    for k, v in all_work.items():
+        # Randomise so we're not doing all the later onces (which need more RAM)
+        # at the same time
+        random.shuffle(v)
+        print(f"muts: {k} has {len(v)}")
+
+    cores_for_muts = {0: 20, 1: 10, 2: 5, 3: 2}
     json_data = []
-    with cf.ProcessPoolExecutor(cores) as executor:
-        futures = [executor.submit(worker, work) for work in easy_work]
-        for future in tqdm.tqdm(cf.as_completed(futures), total=len(easy_work)):
-            result = future.result()
-            json_data.append(result)
-            dump(json_data, output)
+    for muts, cores in cores_for_muts.items():
+        easy_work = all_work[muts]
+        print(f"Running {len(easy_work)} for {muts} mutations")
+        with cf.ProcessPoolExecutor(cores) as executor:
+            futures = [executor.submit(worker, work) for work in easy_work]
+            for future in tqdm.tqdm(cf.as_completed(futures), total=len(easy_work)):
+                result = future.result()
+                json_data.append(result)
+                dump(json_data, output)
 
+    hard_work = []
+    for k, v in all_work.items():
+        if k > max(cores_for_muts.keys()):
+            hard_work.extend(v)
+
+    print(f"Running remaining {len(hard_work)} one by one")
     for work in tqdm.tqdm(hard_work):
         result = worker(work)
         json_data.append(result)
