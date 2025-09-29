1ef3e5f29dffc892a3a3d08387374b3993765ce7
Yan Wong <yan.wong@bdi.ox.ac.uk>
2025-09-29 23:19:55 +0100

    New copying patterns
    And copying_patterns.html file

--- src/save_copying_patterns.py
+++ src/save_copying_patterns.py
@@ -32,7 +32,7 @@ def pangoX_RE_node_labels(exclude_dups=True):
         pango_x_events.closest_recombinant[use],
         'num_descendant_samples'
     ]
-    # Exclude non-recombinants, and ondes where closes RE node is BA.5
+    # Exclude non-recombinants, and nodes where closes RE node is BA.5
     pango_x = pango_x_events[np.logical_and(
         closest_recombinant_num_samples > 0,
         closest_recombinant_num_samples < 1e5
@@ -53,11 +53,7 @@ def pangoX_RE_node_labels(exclude_dups=True):
     else:
         return dict(px)
 
-def save_copying_pattern_image(node_id, label, ts, save_dir, zoom=None,**kwargs):
-    # NB - do not zoom if no ATCG letters are printed, as boxes will be of varying size
-    html_str = sc2ts.info.CopyingTable(ts, node_id).html(**kwargs)
-    html_str = html_str.replace('transform:', '-webkit-transform:')
-    html_str = html_str.replace('writing-mode:', '-webkit-writing-mode:')
+def save_copying_pattern_image(html_str, label, save_dir, zoom=None):
     options = {"format": "png", "quiet": "", "transparent": ""}  # use transparent so we can crop
     if zoom is not None:
         options['zoom'] = zoom 
@@ -69,33 +65,104 @@ def save_copying_pattern_image(node_id, label, ts, save_dir, zoom=None,**kwargs)
     img = img.crop(bbox)
     img.save(save_dir / f"{label}.png", "PNG", optimize=True, compress_level=9)
 
+def get_copying_table(ts, node_id, **kwargs):
+    html_str = sc2ts.info.CopyingTable(ts, node_id).html(**kwargs)
+    html_str = html_str.replace('transform:', '-webkit-transform:')
+    return html_str.replace('writing-mode:', '-webkit-writing-mode:')
+
+def save_copying_table_image(ts, node_id, label, save_dir, zoom=None,**kwargs):
+    # NB - do not zoom if no ATCG letters are printed, as boxes will be of varying size
+    html_str = get_copying_table(ts, node_id, **kwargs)
+    html_str = html_str.replace('<style>', '<style>.copying-table th {text-align: right; font-weight: normal;}')
+    save_copying_pattern_image(html_str, label, save_dir, zoom=zoom)
+
+rec = pd.read_csv(data_dir / "recombinants.csv").set_index('recombinant')
+
+####
+# The pango X versions
+####
 for u, label in tqdm(pangoX_RE_node_labels().items()):
-    save_copying_pattern_image(
-       u,
-       label,
-       ts,
-       png_dir,
+    row = rec.loc[u]
+    save_copying_table_image(
+        ts,
+        u,
+        label,
+        png_dir,
         hide_extra_rows=True,
-        hide_labels=True,
         show_bases=None,
-       font_family='Verdana',
+        child_label=row.recombinant_pango,
+        parent_labels=[row.parent_left_pango, row.parent_right_pango],
+        child_rgt_label=f"&nbsp;{u}",
+        font_family='Verdana',
     )
 
+####
+# The examples for bad quadrants
+####
 for u, lab in tqdm([
     (427863, "RE_node-QCpass-427863"),  # Q1
     (748991, "RE_node-QCfail-748991"),  # Q2
     (411345, "RE_node-QCfail-411345"),  # Q3
     (663484, "RE_node-QCfail-663484"),  # Q4
 ]):
-    save_copying_pattern_image(
+    row = rec.loc[u]
+    save_copying_table_image(
+        ts,
         u,
         lab,
-        ts,
         png_dir,
         hide_extra_rows=False,
-        hide_labels=False,
         show_bases=True,
-        child_label=f"{u}",
         zoom=4,
+        child_label=row.recombinant_pango,
+        parent_labels=[row.parent_left_pango, row.parent_right_pango],
+        child_rgt_label=f"&nbsp;Child&nbsp;node&nbsp;#{u}",
         font_family='Verdana',
     )
+
+####
+# The html file (and all recombs)
+####
+with open(data_dir / "copy_patterns.html", "wt") as f:
+    print(
+        "<html>",
+        "<head><style>",
+        "@media print {@page {size: A3 landscape;}}", 
+        "table tr td, table tr th {page-break-inside: avoid;}",
+        ".nobreak {page-break-inside: avoid !important; margin-bottom: 5px}",
+        ".fail-lft {background-color: gainsboro; background-image: repeating-linear-gradient(-45deg, transparent, transparent 5px, silver 5px, silver 6px);}",
+        ".fail-rgt {background-color: gainsboro; background-image: repeating-linear-gradient(45deg, transparent, transparent 5px, silver 5px, silver 6px);}",
+        ".fail-lft.fail-rgt {background-color: gainsboro; background-image: repeating-linear-gradient(-45deg, transparent, transparent 5px, silver 5px, silver 6px), repeating-linear-gradient(45deg, transparent, transparent 5px, silver 5px, silver 6px);}",
+        "</style></head>",
+        "<body>",
+        sep="\n",
+        file=f
+    )
+    df = rec.sort_index()
+    for i, row in tqdm(enumerate(df.itertuples())):
+        css_classes = ["nobreak"]
+        if row.net_min_supporting_loci_lft < 4:
+            css_classes.append("fail-lft")
+        if row.net_min_supporting_loci_rgt < 4:
+            css_classes.append("fail-rgt")
+        if i == 0:
+            exclude_stylesheet = False
+            child_rgt_label = f'<span style="white-space: pre;"> Copying pattern for recombinant child, node #{row.Index}</span>'
+        else:
+            exclude_stylesheet = True
+            child_rgt_label = f"&nbsp;#{row.Index}"
+        t = get_copying_table(
+            ts,
+            row.Index,
+            child_label=row.recombinant_pango,
+            parent_labels=[row.parent_left_pango, row.parent_right_pango],
+            child_rgt_label=child_rgt_label,
+            exclude_stylesheet=i>0,
+        )
+        print(f'<div class="{" ".join(css_classes)}">{t}</div>', file=f)
+        #save_copying_pattern_image(
+        #    t, f"RE_node-QC{s}-{row.recombinant}",
+        #    png_dir, child_label=row.sample_id, hide_extra_rows=False, hide_labels=False,
+        #    show_bases=True, zoom=4, font_family='Verdana'
+        #)
+    print("</body>", "</html>", sep="\n", file=f)
